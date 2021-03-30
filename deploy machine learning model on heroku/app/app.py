8a7fbfe0add596e3f1bdca90d286bce2bc563828
Gregory Hogg <gahogg@edu.uwaterloo.ca>
2021-03-30 00:11:45 -0400

    Working on local

--- deploy machine learning model on heroku/app/app.py
+++ deploy machine learning model on heroku/app/app.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 from joblib import dump, load
-from flask import render_template, request, make_response
+from flask import url_for, render_template, request, send_from_directory, make_response
 from flask import Flask
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import plotly
-plotly.io.orca.ensure_server()
+from os.path import dirname, join
+import uuid
 
 app = Flask(__name__)
 loaded_model = load('model.joblib')
@@ -15,16 +16,17 @@ loaded_model = load('model.joblib')
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'GET':
-        return render_template('main.html', file_name="base_pic.png")
+        return render_template('main.html', href=url_for('static', filename="base_pic.svg"))
     else:
         text = request.form['text']
-        np_arr = integers_string_to_input_arr(text)
-        make_picture('AgesAndHeights.pkl', np_arr, loaded_model)
-        return render_template('main.html', file_name='predictions_pic.png')
-
+        np_arr = floats_string_to_input_arr(text)
+        u = uuid.uuid4()
+        random_url = join("static/", u.hex + ".svg")
+        make_picture('AgesAndHeights.pkl', np_arr, loaded_model, output_file=random_url)
+        return render_template('main.html', href=random_url)
 
 # After Step 3
-def make_picture(training_data_filename, new_input_arr, model, output_file='predictions_pic.png'):
+def make_picture(training_data_filename, new_input_arr, model, output_file):
   # Plot training data with model
   data = pd.read_pickle('AgesAndHeights.pkl')
   x_new = np.arange(18).reshape((18, 1))
@@ -47,10 +49,10 @@ def make_picture(training_data_filename, new_input_arr, model, output_file='pred
                 color='purple',
                 width=2
             ))))
-  fig.write_image(output_file, width=1200)
+  fig.write_image(output_file, width=800, engine='kaleido')
   return fig
 
-def integers_string_to_input_arr(int_str):
-  integers = [int(x) for x in int_str.split(',')]
-  as_np_arr = np.array(integers).reshape(len(integers), 1)
+def floats_string_to_input_arr(floats_str):
+  floats = [float(x.strip()) for x in floats_str.split(',')]
+  as_np_arr = np.array(floats).reshape(len(floats), 1)
   return as_np_arr
